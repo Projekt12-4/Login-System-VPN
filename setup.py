fa290246026b8d718353085f52d76d79bce2fcf8
xqly-stack <112006489+xqly-stack@users.noreply.github.com>
2022-11-18 18:59:54 +0000

    changed setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 def install():
     install_dependencies_linux = "pip3 install -r requirements.txt"
     os.system(install_dependencies_linux)
-    print ("It is strongly advised that you run start.py thereafter!")
 
 if __name__ == "__main__":
 
     install()
+    print ("It is strongly advised that you run start.py thereafter!")
 
