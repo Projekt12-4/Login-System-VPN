13efc6e3395be4fbbbfc6c18b179565224b7e6a4
xqly-stack <oleg.ionov@tutanota.de>
2022-12-06 21:24:44 +0000

    added some changes

--- website/models.py
+++ website/models.py
@@ -15,11 +15,12 @@ from dataclasses import dataclass, field
 
 @dataclass
 class User(db.Model, UserMixin):
+    uid: str = db.Column(db.String(200), unique=True)
     id: int = db.Column(db.Integer, primary_key=True)
     email: str = db.Column(db.String(150), unique=True)
     password: str = db.Column(db.String(150))
     first_name: str = db.Column(db.String(150))
-    key_file: str = db.Column(db.String(200), unique=True)
+    key_file: str = db.Column(db.String(500)) # Saves the *.txt version of the key_file
 
 
 
