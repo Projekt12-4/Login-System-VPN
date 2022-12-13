16aeb1157af7ebdcf7eb42f4aee76de6f4eb7a97
xqly-stack <oleg.ionov@tutanota.de>
2022-12-13 17:34:44 +0000

    Switched from salted sha256 hashing to argon2 hashing

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
+from passlib.hash import argon2
 from flask_login import login_user, login_required, logout_user, current_user
 import bleach
+from . import db
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -20,7 +20,7 @@ def login():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            if check_password_hash(user.password, password):
+            if argon2.verify(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
@@ -65,8 +65,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-            password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=argon2.hash(password1))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
