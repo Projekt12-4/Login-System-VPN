6562676ca75d0ad3d017465891d3720bce5da44e
xqly-stack <112006489+xqly-stack@users.noreply.github.com>
2023-01-07 21:52:29 +0100

    Removed wireguard

--- website/auth.py
+++ website/auth.py
@@ -5,7 +5,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 import bleach
-from wireguard import Server
 
 auth = Blueprint('auth', __name__)
 
